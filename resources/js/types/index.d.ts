cef6736883c831b45223d0f1be33e5b4f266bdd8
Ishtehar Hussain <x.e.q.tionrz@gmail.com>
2025-08-24 20:27:49 +0600

    Add ContainerItem type

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -1,62 +1,62 @@
-import { LucideIcon } from 'lucide-react';
-import type { Config } from 'ziggy-js';
+import { LucideIcon } from 'lucide-react'
+import type { Config } from 'ziggy-js'
 
 export interface Auth {
-    user: User;
+    user: User
 }
 
 export interface BreadcrumbItem {
-    title: string;
-    href: string;
+    title: string
+    href: string
 }
 
 export interface NavGroup {
-    title: string;
-    items: NavItem[];
+    title: string
+    items: NavItem[]
 }
 
 export interface NavCollapseGroup {
-    title: string;
-    icon?: LucideIcon | null;
-    links: NavItem[];
+    title: string
+    icon?: LucideIcon | null
+    links: NavItem[]
     isActive?: boolean
 }
 
 export interface NavItem {
-    title: string;
-    href: string;
-    icon?: LucideIcon | null;
-    isActive?: boolean;
+    title: string
+    href: string
+    icon?: LucideIcon | null
+    isActive?: boolean
 }
 
 export interface SharedData {
-    name: string;
-    quote: { message: string; author: string };
-    auth: Auth;
-    ziggy: Config & { location: string };
-    sidebarOpen: boolean;
-    [key: string]: unknown;
+    name: string
+    quote: { message: string; author: string }
+    auth: Auth
+    ziggy: Config & { location: string }
+    sidebarOpen: boolean
+    [key: string]: unknown
 }
 
 export interface PaginationLinkData {
-    active: boolean;
-    url: string;
+    active: boolean
+    url: string
     label: string
 }
 
 export interface PaginationMeta {
-    total: number;
-    current_page: number;
-    from: number;
-    to: number;
+    total: number
+    current_page: number
+    from: number
+    to: number
     links: PaginationLinkData[]
-    per_page: number;
+    per_page: number
 }
 
 export type StrOrNum = string | number
 interface Option {
-    value: StrOrNum,
-    label: string,
+    value: StrOrNum
+    label: string
 }
 
 export type Filter = [string, string, string]
@@ -70,23 +70,23 @@ export type FilterTransformed = [
 
 
 export interface FilterConfig {
-    key: string,
-    label: string,
-    dataType: 'float' | 'int' | 'string',
-    inputType: 'select' | 'date' | 'number' | 'text',
-    group: string,
-    getOptions?: (apiToken: string) => ((search: string) => Promise<Option[]>),
+    key: string
+    label: string
+    dataType: 'float' | 'int' | 'string'
+    inputType: 'select' | 'date' | 'number' | 'text'
+    group: string
+    getOptions?: (apiToken: string) => ((search: string) => Promise<Option[]>)
     ops?: FilterOperator[]
 }
 export interface User {
-    id: number;
-    name: string;
-    email: string;
-    avatar?: string;
-    email_verified_at: string | null;
-    created_at: string;
-    updated_at: string;
-    [key: string]: unknown; // This allows for additional properties...
+    id: number
+    name: string
+    email: string
+    avatar?: string
+    email_verified_at: string | null
+    created_at: string
+    updated_at: string
+    [key: string]: unknown // This allows for additional properties...
 }
 
 export interface LetterOfCredit {
@@ -129,18 +129,25 @@ export type ProformaInvoiceItem = Tyre & {
     unit_price: number
 }
 
+export type ContainerItem = ProformaInvoiceItem & {
+    container_num: string
+    bol: string
+    total_tax: number
+    total_weight: number
+}
+
 export interface LetterOfCreditFormErrors {
-    lc_num?: string,
-    date_issued?: string,
-    date_expiry?: string,
-    applicant?: string,
-    beneficiary?: string,
-    port_depart?: string,
-    port_arrive?: string,
-    currency_code?: string,
-    exchange_rate?: string,
-    foreign_amount?: string,
-    foreign_expense?: string,
-    domestic_expense?: string,
-    notes?: string,
+    lc_num?: string
+    date_issued?: string
+    date_expiry?: string
+    applicant?: string
+    beneficiary?: string
+    port_depart?: string
+    port_arrive?: string
+    currency_code?: string
+    exchange_rate?: string
+    foreign_amount?: string
+    foreign_expense?: string
+    domestic_expense?: string
+    notes?: string
 }
